9b7eea8656df5929921ed075f6e18efd751989fa
FranklinSilva <--global>
2015-08-06 21:07:07 -0300

    added div container to modal

--- src/br-browser-update.js
+++ src/br-browser-update.js
@@ -42,6 +42,7 @@
     
     function pushHtml(){
       var html = [];
+      html.push('<div id="br-browser-update">');
       html.push('<div class="modal-body">');
       html.push('<h1>O seu navegador é antigo. =(</h1>');
       html.push('<p>Para obter melhor eperiência de uso do sistema faça o download de um dos navegadores abaixo.</p>');
@@ -49,7 +50,7 @@
       html.push('<div class="stand-column"><a href="https://www.google.com/chrome/browser/"><img src="../src/assets/chrome-logo.png" /></a></div>');
       html.push('<div class="stand-column"><a href="https://www.mozilla.org/pt-BR/"><img src="../src/assets/firefox-logo.png" /></a></div>');
       html.push('<div class="stand-column"><a href="http://www.opera.com/pt-br"><img src="../src/assets/opera-logo.png" /></a></div>');
-      html.push('</div></div>');
+      html.push('</div></div></div>');
       //var target = document.getElementById("br-browser-update");
       var target = document.getElementsByTagName('body')[0];
       target.innerHTML = html.join('');
